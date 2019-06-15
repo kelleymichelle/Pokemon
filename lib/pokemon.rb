7db2383a0b5659c0676af6d8edac9d4c84f556f4
Kelley Chaplain <kmchaplain@gmail.com>
2019-06-15 18:29:44 -0500

    Pilot commit

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,8 @@
-require "pokemon/version"
+require_relative "./pokemon/version"
 
-module Pokemon
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# module Pokemon
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
+
+require_relative './pokemon/cli'
---
+++ lib/pokemon/cli.rb
@@ -0,0 +1,8 @@
+#CLI controller
+class Pokemon::CLI
+
+  def call
+    puts "Gotta catch em all"
+  end
+  
+end  
