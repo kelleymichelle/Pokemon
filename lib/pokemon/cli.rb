b877e706a05daf5cab4122017801f112348799db
Kelley Chaplain <kmchaplain@gmail.com>
2019-06-15 20:13:09 -0500

    realistic gameplay

--- lib/pokemon/cli.rb
+++ lib/pokemon/cli.rb
@@ -4,28 +4,46 @@ class Pokemon::CLI
   def call
     welcome
     name
+    poke_list
   end
 
   def welcome
-    Catpix::print_image "/Users/kelleychaplain/desktop/ProfOak.gif" 
-    puts <<-DOC
-    Hello There
-    Welcome To The World Of Pokemon
-    My name is OAK
-    People often refer to me as the Pokemon PROF
-    This World is inhabited by creatures known as Pokemon
-    For Some People Pokemon are Pets
-    Others use them for battle
-    As for myself 
-    I study Pokemon as a profession
-    DOC 
+    # Catpix::print_image "/Users/kelleychaplain/desktop/ProfOak.gif" 
+  
+    puts "Hello There"
+    puts "Welcome To The World Of Pokemon"
+    sleep 1
+    puts "My name is OAK"
+    puts "People often refer to me as the Pokemon PROF"
+    sleep 1
+    puts "This World is inhabited by creatures known as Pokemon"
+    puts "For Some People Pokemon are Pets"
+    puts "Others use them for battle"
+    puts "As for myself" 
+    puts "I study Pokemon as a profession"
+    sleep 1
+   
   end
   
   def name
     puts "But tell me about yourself, What is your name?"
     reply = gets.chomp
     puts "#{reply}, Your very own Pokemon Legend is about to begin! A world full of dreams and adventures and Pokemon."
+    sleep 1
     puts "Let's Go!"
+    sleep 1
+  end
+  
+  def poke_list
+    arr = ["charmander", "bulbasaur", "pikachu"]
+
+    puts "... ... ..."
+    sleep 2
+    puts "... ... ..."
+    sleep 2
+    puts "A wild pokemon is attacking!"
+    sleep 2
+    puts arr.sample
   end  
 
   
