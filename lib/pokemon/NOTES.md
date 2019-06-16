b810af0ea2e79966b8bacae99773bb152383b536
Kelley Chaplain <kmchaplain@gmail.com>
2019-06-15 20:05:51 -0500

    [first commit]

---
+++ lib/pokemon/NOTES.md
@@ -0,0 +1,3 @@
+1. Welcome pokemon trainer
+2. Gives a list of random pokemon and asks which they want to catch
+3. prints picture of pokemon to terminal
--- lib/pokemon/cli.rb
+++ lib/pokemon/cli.rb
@@ -1,8 +1,32 @@
-#CLI controller
+require 'catpix'
 class Pokemon::CLI
 
   def call
-    puts "Gotta catch em all"
+    welcome
+    name
   end
+
+  def welcome
+    Catpix::print_image "/Users/kelleychaplain/desktop/ProfOak.gif" 
+    puts <<-DOC
+    Hello There
+    Welcome To The World Of Pokemon
+    My name is OAK
+    People often refer to me as the Pokemon PROF
+    This World is inhabited by creatures known as Pokemon
+    For Some People Pokemon are Pets
+    Others use them for battle
+    As for myself 
+    I study Pokemon as a profession
+    DOC 
+  end
+  
+  def name
+    puts "But tell me about yourself, What is your name?"
+    reply = gets.chomp
+    puts "#{reply}, Your very own Pokemon Legend is about to begin! A world full of dreams and adventures and Pokemon."
+    puts "Let's Go!"
+  end  
+
   
 end  
