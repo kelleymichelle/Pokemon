6fb8cb798a7ce61a0afede822a35cae2b75fcbf9
Kelley Chaplain <kmchaplain@gmail.com>
2019-07-08 14:53:31 -0500

    fixed more info link

--- lib/pokemon/scraper.rb
+++ lib/pokemon/scraper.rb
@@ -5,32 +5,23 @@ require 'nokogiri'
 class Scraper
 
   def self.scrapeyscrape
-    
     new = []
     url = 'https://pokemondb.net/pokedex/national'
     doc = Nokogiri::HTML(open(url))
-    # binding.pry
-    # doc.css('h2').each do |gen|
-    # if new.count < 151
+   
       doc.css('.infocard').each do |this|
-        # binding.pry
-        # generation = gen.text.split(" ")[1]
+       
         types = []
         x = this.css('.img-sprite') 
+          org_url = 'https://pokemondb.net/pokedex/national'
           image = x.attr('data-src').value
           name = this.css('.ent-name').text
-          more_info = this.css('a')[0].attr('href')
+          more_info = org_url.concat(this.css('a')[0].attr('href'))
           this.css('small a').each {|onetype| types << onetype.text }
             new << Pokemon::Pokemon_list.new(name, image, types, more_info)
-
-      # end  
+      
     end
     return " ^ - ^ "
   end  
 
 end  
-
-#name = this.css('.ent-name').text
-#type = this.css('small a').text
-#image = x = this.css('.img-sprite') 
-#             x.attr('data-src').value
